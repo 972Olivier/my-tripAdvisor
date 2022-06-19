04084ab7e2f89ea1036fadc9914a234385d17959
Olivier <olivier972@MacBook-Pro-de-Olivier.local>
2022-06-19 16:58:10 -0400

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", () => {
+  //   console.log("chargé youpi c'est super");
+
+  document.getElementById("button-head").addEventListener("click", () => {
+    // console.log("clic");
+    document.getElementById("form-submit").classList.remove("hidden");
+    document.getElementById("body").classList.add("no-scrool");
+  });
+
+  document.getElementById("x").addEventListener("click", () => {
+    document.getElementById("form-submit").classList.add("hidden");
+    document.getElementById("body").classList.remove("no-scrool");
+  });
+
+  document.querySelector("#form").addEventListener("submit", async (event) => {
+    event.preventDefault();
+    // console.log("submit");
+    const data = {
+      firstname: document.getElementById("firstname").value,
+      lastname: document.getElementById("lastname").value,
+      email: document.getElementById("email").value,
+      message: document.getElementById("textaera").value,
+    };
+    const response = await axios.post("http://localhost:3000/form", data);
+
+    console.log(response);
+    document.getElementById("form-submit").classList.add("hidden");
+    document.getElementById("body").classList.remove("no-scrool");
+  });
+});
